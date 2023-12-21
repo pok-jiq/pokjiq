f28f9057e7a8a391469f31085c0388ed0764338a
pok-jiq <71089818+pok-jiq@users.noreply.github.com>
2023-12-22 03:46:41 +0800

    Update search.py

--- bot/helper/mirror_utils/gdrive_utils/search.py
+++ bot/helper/mirror_utils/gdrive_utils/search.py
@@ -142,7 +142,7 @@ class gdSearch(GoogleDriveHelper):
                     msg += f"📁 <code>{file.get('name')}<br>(folder)</code><br>"
                     msg += f"<b><a href={furl}>Drive Link</a></b>"
                     if index_url:
-                        url = f'{index_url}findpath?id={file.get("id")}'
+                        url = f'{index_url}/{url_path}/'
                         msg += f' <b>| <a href="{url}">Index Link</a></b>'
                 elif mime_type == "application/vnd.google-apps.shortcut":
                     furl = self.G_DRIVE_DIR_BASE_DOWNLOAD_URL.format(file.get("id"))
@@ -155,7 +155,7 @@ class gdSearch(GoogleDriveHelper):
                     msg += f"📄 <code>{file.get('name')}<br>({get_readable_file_size(int(file.get('size', 0)))})</code><br>"
                     msg += f"<b><a href={furl}>Drive Link</a></b>"
                     if index_url:
-                        url = f'{index_url}findpath?id={file.get("id")}'
+                        url = f'{index_url}/{url_path}'
                         msg += f' <b>| <a href="{url}">Index Link</a></b>'
                         if mime_type.startswith(("image", "video", "audio")):
                             urlv = f'{index_url}findpath?id={file.get("id")}&view=true'
